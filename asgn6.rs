70b1caddc40513cc38190a2f92bef8988bd011b4
gponomar <gponomar@calpoly.edu>
2018-11-26 18:30:51 -0800

    started structs and funcs for numbers

--- asgn6.rs
+++ asgn6.rs
@@ -1,11 +1,71 @@
+use std::collections::HashMap;
+//let mut topenv = HashMap<String, Value>::new();
+
 struct NumC {
-    n: i32;
+    n: i32
 }
 
 struct IdC {
-    s: &'a str;
+    s: String
 }
 
 struct BoolC {
-    b: bool;
+    b: bool
+}
+
+struct NumV {
+	n: i32
+}
+
+struct StrV {
+	s: String
+}
+
+struct CloV {
+	
+}	
+
+struct PrimV {
+	
+}	
+
+struct Env{
+	hm: HashMap<String, NumV>
+}
+
+trait ExprC {
+	fn new(n: i32) -> Self;
+	fn interp(&self)-> impl Value;
+}
+
+trait Value {
+	fn new (n: i32) -> Self;
+	fn serialize(&self)-> String;
+}
+
+
+impl ExprC for NumC {
+	fn new(n: i32) -> Self
+	{
+		NumC {n: n}
+	}
+	fn interp(&self) -> impl Value
+	{
+		NumV::new(self.n)
+	}
+}
+
+impl Value for NumV {
+	fn new(n: i32) -> Self
+	{
+		NumV {n: n}
+	}
+	fn serialize(&self) -> String
+	{
+		self.n.to_string()
+	}
+}
+
+fn main() {
+    println!("Hello World!");
 }
